a0c816a85894e4febf0df00c8e07d9a8ed573bad
jonmes <mesfinjohn@rocketmail.com>
2022-07-10 22:30:03 +0300

    reading intersection working

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+onMounted(() => {
+  const observer = new IntersectionObserver((entries) => {
+    console.log('entries value: ', entries);
+  })
+
+  document.querySelectorAll(".card").forEach((selection) => {
+    observer.observe(selection)
+  })
+});
+</script>
+
+<template>
+  <div>
+    <div class="card-container">
+      <div class="card">this is first card</div>
+    </div>
+  </div>
+</template>
